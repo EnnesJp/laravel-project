006194101af0a41ff1860bcda63700330eadadb8
Joao Ennes <joaopedroennes@outlook.com>
2025-12-18 16:35:59 -0300

    feat: add user new fields

--- database/migrations/2025_12_18_182711_add_document_and_role_to_users_table.php
+++ database/migrations/2025_12_18_182711_add_document_and_role_to_users_table.php
@@ -12,7 +12,7 @@ return new class () extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('document', 14);
+            $table->string('document', 14)->unique();
             $table->enum('role', UserRole::values());
         });
     }
