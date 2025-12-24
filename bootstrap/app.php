16a11a008c14ea7fc74bbd89e9ecc2d2c7344dbb
Joao Ennes <joaopedroennes@outlook.com>
2025-12-24 12:19:22 -0300

    feat: add rate limiter to api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->throttleApi('60,1');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
